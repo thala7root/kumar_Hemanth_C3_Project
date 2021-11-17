fb21e673eeb1783c2f353d7c60a620c14270dee6
thala7root <92441183+thala7root@users.noreply.github.com>
2021-11-17 22:10:43 +0530

    Update RestaurantTest.java
    Part 3 : Solution

--- src/test/java/RestaurantTest.java
+++ src/test/java/RestaurantTest.java
@@ -70,10 +70,10 @@ class RestaurantTest {
                 ()->restaurant.removeFromMenu("French fries"));
 
         @Test
-           /* public void calculateOrderTotal_should_return_388_after_adding_the_119_and_269(){
+            public void calculateOrderTotal_should_return_388_after_adding_the_119_and_269(){
                 Integer orderTotal = restaurant.calculateOrderTotal(119,269);
                 assertEquals(388,orderTotal);
-            }*/
+            }
     }
     //<<<<<<<<<<<<<<<<<<<<<<<MENU>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 }
